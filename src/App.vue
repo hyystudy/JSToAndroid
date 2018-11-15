cb9ac2810b8d13da6866ce0b95236bbca4d653d4
SJKJheyangyang <heyangyang@digitalplus.io>
2018-11-16 01:01:50 +0800

    first commit and done

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+  var helloComponent;
+  export default {
+    name: 'App',
+    methods: {
+      setComponent: function (component) {
+        helloComponent = component
+      },
+      }
+  }
+
+  window['receiveMsg'] = function (msg) {
+    helloComponent.changHello(msg)
+  }
+
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <div>{{hello}}</div>
+    <button @click="callAndroid">调用Android方法</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      hello: 'Hello World',
+    }
+  },
+  methods: {
+    changHello: function (msg) {
+      this.hello = msg
+    },
+    callAndroid: function (){
+      //调用Android的方法 提前需要和Android端对接一下
+      window.android.showInfoFromJs('你好我来自web端')
+    }
+  },
+  created() {
+    this.$parent.setComponent(this)
+  }
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
